eac6d227ec2dd5c11f17cb921aca3b098d1b4e05
antogit-sys <antonioperrgiorgi@gmail.com>
2024-03-06 19:52:29 +0100

    create help

--- shaur.pl
+++ shaur.pl
@@ -8,14 +8,37 @@
 
 use strict;
 use warnings;
+use feature 'say';
+use Term::ANSIColor qw(:constants);
+
 
 sub main{ my $ARGC = scalar @ARGV;
-    print("hello");
     if($ARGC == 0){
-        #help()
+        help()
 	}else{
         #my %options=();
     }
+}
+sub help{
+    say "";
+    say BOLD, CYAN, "="x22, " Simple Helper AUR ", "="x22, RESET;    
+    my $options = <<END
+
+    \tDescription:  
+    \t\t shaur is at Simple Helper AUR for Arch-Linux
+    \tUsage:
+    \t\t shaur <option> <package>
+    \tOption;
+    \t\t -s \t(search package)
+    \t\t -si\t(search & install)
+    \t\t -l \t(get list package)
+    \t\t -lu\t(get list package and update)
+    \t\t -r \t(remove package)
+    \tExample:
+    \t\t shaur -s lol
+END
+;
+    say "$options";
 }
 
 main()
