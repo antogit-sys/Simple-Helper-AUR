0a6d5c295c2a18f599c102d896e302598247102b
antogit-sys <antonioperrgiorgi@gmail.com>
2024-03-06 19:23:12 +0100

    create semi-main

---
+++ shaur.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+#
+#   Nickanme:   Aska
+#   GitHub:     https://github.com/antogit-sys
+#   LICENSE:    GPLv3 
+#
+
+use strict;
+use warnings;
+
+sub main{ my $ARGC = scalar @ARGV;
+    print("hello");
+    if($ARGC == 0){
+        #help()
+	}else{
+        #my %options=();
+    }
+}
+
+main()
