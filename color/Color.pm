20f2facfac4cae6709903f232dd99ad59506423d
antogit-sys <antonioperrgiorgi@gmail.com>
2024-03-07 19:21:21 +0100

    create Color.pm

---
+++ color/Color.pm
@@ -0,0 +1,27 @@
+# File: color/Color.pm
+
+package Color;
+
+use strict;
+use warnings;
+
+use Exporter 'import'; # or use base 'Exporter';
+
+# - declare constant
+use constant{
+     BOLD  => "\e[1m",
+     CYAN  => "\e[36m",
+     RED   => "\e[31m",
+     WHITE => "\e[37m",
+     RESET => "\e[0m"
+};
+
+# - create alias list
+our @EXPORT_OK = qw(BOLD CYAN WHITE RED RESET);
+
+# - create tag
+our %EXPORT_TAGS = (
+    consts => ['BOLD', 'CYAN', 'WHITE', 'RED','RESET']
+);
+
+1;
