6c819234cab03b06bd26599d0f1748a139cb0e6e
antogit-sys <antonioperrgiorgi@gmail.com>
2024-03-15 20:56:31 +0100

    set BLUE

--- color/Color.pm
+++ color/Color.pm
@@ -13,20 +13,21 @@ use Exporter 'import'; # or use base 'Exporter';
 
 # - declare constant
 use constant{
-     BOLD  =>  "\e[1m",
-     CYAN  =>  "\e[36m",
-     RED   =>  "\e[31m",
-     YELLOW => "\e[33m",
-     WHITE =>  "\e[37m",
-     RESET =>  "\e[0m"
+     BOLD   =>  "\e[1m",
+     CYAN   =>  "\e[36m",
+     RED    =>  "\e[31m",
+     YELLOW =>  "\e[33m",
+     WHITE  =>  "\e[37m",
+     BLUE   =>  "\e[34m",
+     RESET  =>  "\e[0m"
 };
 
 # - create alias list
-our @EXPORT_OK = qw(BOLD CYAN WHITE RED RESET YELLOW);
+our @EXPORT_OK = qw(BOLD CYAN WHITE RED RESET YELLOW BLUE);
 
 # - create tag
 our %EXPORT_TAGS = (
-    consts => ['BOLD', 'CYAN', 'WHITE', 'RED','RESET', 'YELLOW']
+    consts => ['BOLD', 'CYAN', 'WHITE', 'RED','RESET', 'YELLOW', 'BLUE']
 );
 
 1; #exit succefull
