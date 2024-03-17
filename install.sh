b5df637e3b534399a8d7882f68b130182d5763d4
antogit-sys <antonioperrgiorgi@gmail.com>
2024-03-17 22:31:18 +0100

    fix bug user root

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 
+# - declare color constants
 declare -r RED='\033[1;31m'
 declare -r LGREEN='\033[1;92m'
-#declare -r LYELLOW='\033[1;33m'
 declare -r LCYAN='\033[0;96m'
 declare -r BOLD='\033[1m'
 declare -r NC='\033[0m' # No Color
 
 function main {
-	if [ $UID != 0 ]
+	if [ $UID == 0 ]
 	then
-		printf "${RED}[!]This script can only be used by the root user ${NC}\n"
+		printf "${RED}[!] Error this script can only be used by the normal user ${NC}\n"
 		
 	else
 		message_box
@@ -21,7 +21,7 @@ exit
 }
 
 function install_cpan() {
-    printf "${LCYAN}${BOLD}[!]Installing SHAUR for Arch Linux\n"
+    printf "${LCYAN}${BOLD}[!] Installing SHAUR for Arch Linux\n"
     echo -e "-----------------------------------${NC}";
     printf "$LCYAN[+] Installing necessary Perl modules...${NC}\n"
     cpan install JSON Env
@@ -35,19 +35,20 @@ function install_cpan() {
 function message_box() {
     dialog --backtitle "Setup install SHAUR" --title "Simple Helper AUR install" \
 	--msgbox "this tool is covered by the gpl3 license\n
-		\ngithub: antogit-sys\n\n\n
+		\ngithub: antogit-sys\n\n\n      
 		proceed with the installation?..." \
 	10 50
 	clear
 }
 
 execute() {
-	pkg_installed_dir="/home/$SUDO_USER/.shaur/pkg-installed"
-
-    printf "${LCYAN}[+]create shaur command${NC}\n";
+    pkg_installed_dir="$HOME/.shaur/pkg-installed"
+    
+    printf "${LCYAN}[+] create shaur command${NC}\n";
+    sudo printf ""
     printf "Copying shaur.pl to... "
 	sleep 1
-	cp shaur.pl /usr/local/bin/shaur
+	sudo cp shaur.pl /usr/local/bin/shaur
     if [ $? -ne 0 ]; then
         printf "${RED}[!] Error moving shaur.pl.${NC}\n"
         exit 1
@@ -56,7 +57,7 @@ execute() {
 
     printf "Copying shaurFunction recursively to... "
 	sleep 2
-	cp -r shaurFunctions /usr/local/bin/shaurFunctions
+	sudo cp -r shaurFunctions /usr/local/bin/shaurFunctions
     if [ $? -ne 0 ]; then
         printf "${RED}[!] Error copying shaurFunctions.${NC}\n"
         exit 1
@@ -65,9 +66,10 @@ execute() {
 
     printf "shaur is... "
     sleep 1
-    chmod a+x /usr/local/bin/shaur
+    sudo chmod a+x /usr/local/bin/shaur
     printf "${LGREEN}excutable$NC\n\n"
     
+    printf "$LCYAN[+] create config file$NC\n"
     if [ ! -d "$pkg_installed_dir" ]; then
         # If it doesn't exist, create it
         mkdir -p "$pkg_installed_dir"
@@ -78,6 +80,5 @@ execute() {
     printf "${LCYAN}${BOLD}Installation completed.${NC}\n\n"
 	
 }
-
 
 main
