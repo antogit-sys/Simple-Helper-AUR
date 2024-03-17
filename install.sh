7cc9b6dbc77f99763e99a41b24a783ead3f32986
antogit-sys <antonioperrgiorgi@gmail.com>
2024-03-17 17:57:51 +0100

    create install.sh

---
+++ install.sh
@@ -0,0 +1,83 @@
+
+declare -r RED='\033[1;31m'
+declare -r LGREEN='\033[1;92m'
+#declare -r LYELLOW='\033[1;33m'
+declare -r LCYAN='\033[0;96m'
+declare -r BOLD='\033[1m'
+declare -r NC='\033[0m' # No Color
+
+function main {
+	if [ $UID != 0 ]
+	then
+		printf "${RED}[!]This script can only be used by the root user ${NC}\n"
+		
+	else
+		message_box
+        install_cpan
+		execute
+	fi
+
+exit
+}
+
+function install_cpan() {
+    printf "${LCYAN}${BOLD}[!]Installing SHAUR for Arch Linux\n"
+    echo -e "-----------------------------------${NC}";
+    printf "$LCYAN[+] Installing necessary Perl modules...${NC}\n"
+    cpan install JSON Env
+    if [ $? -ne 0 ]; then
+        printf "${RED}[!] Error installing Perl modules.${NC}\n"
+        exit 1
+    fi
+    echo "" 
+}
+
+function message_box() {
+    dialog --backtitle "Setup install SHAUR" --title "Simple Helper AUR install" \
+	--msgbox "this tool is covered by the gpl3 license\n
+		\ngithub: antogit-sys\n\n\n
+		proceed with the installation?..." \
+	10 50
+	clear
+}
+
+execute() {
+	pkg_installed_dir="/home/$SUDO_USER/.shaur/pkg-installed"
+
+    printf "${LCYAN}[+]create shaur command${NC}\n";
+    printf "Copying shaur.pl to... "
+	sleep 1
+	cp shaur.pl /usr/local/bin/shaur
+    if [ $? -ne 0 ]; then
+        printf "${RED}[!] Error moving shaur.pl.${NC}\n"
+        exit 1
+    fi
+    printf "$LGREEN/usr/local/bin/shaur${NC}\n"
+
+    printf "Copying shaurFunction recursively to... "
+	sleep 2
+	cp -r shaurFunctions /usr/local/bin/shaurFunctions
+    if [ $? -ne 0 ]; then
+        printf "${RED}[!] Error copying shaurFunctions.${NC}\n"
+        exit 1
+    fi
+    printf "$LGREEN/usr/local/bin/shaurFunctions${NC}\n"
+
+    printf "shaur is... "
+    sleep 1
+    chmod a+x /usr/local/bin/shaur
+    printf "${LGREEN}excutable$NC\n\n"
+    
+    if [ ! -d "$pkg_installed_dir" ]; then
+        # If it doesn't exist, create it
+        mkdir -p "$pkg_installed_dir"
+        echo -e "Directory '$pkg_installed_dir' created successfully.\n"
+    else
+        echo -e "Directory '$pkg_installed_dir' already exists.\n"
+    fi
+    printf "${LCYAN}${BOLD}Installation completed.${NC}\n\n"
+	
+}
+
+
+main
